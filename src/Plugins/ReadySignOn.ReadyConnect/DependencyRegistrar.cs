1013690532ddfe66ae2bec1e64ad64b3c011539c
mridentity <mridentity>
2019-11-22 18:03:43 -0500

    Added missing view files and dpendency registeration.

---
+++ src/Plugins/ReadySignOn.ReadyConnect/DependencyRegistrar.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using ReadySignOn.ReadyConnect.Core;
+using SmartStore.Core.Infrastructure;
+using SmartStore.Core.Infrastructure.DependencyManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadySignOn.ReadyConnect
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, bool isActiveModule)
+        {
+            builder.RegisterType<ReadyConnectProviderAuthorizer>().As<IOAuthProviderReadyConnectAuthorizer>().InstancePerRequest();
+        }
+
+        public int Order
+        {
+            get { return 1; }
+        }
+    }
+}
