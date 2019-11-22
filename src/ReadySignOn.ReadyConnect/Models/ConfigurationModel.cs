cf208b8cc6f848b17d6cd9d2fc26a1da088dc8f7
mridentity <mridentity>
2019-11-22 16:21:08 -0500

    Add Settings and Models for ReadyConnect plugin.

---
+++ src/ReadySignOn.ReadyConnect/Models/ConfigurationModel.cs
@@ -0,0 +1,17 @@
+﻿using SmartStore.Web.Framework;
+using SmartStore.Web.Framework.Modelling;
+
+namespace ReadySignOn.ReadyConnect.Models
+{
+    public class ConfigurationModel : ModelBase
+    {
+        [SmartResourceDisplayName("Plugins.ExternalAuth.ReadyConnect.ClientKeyIdentifier")]
+        public string ClientKeyIdentifier { get; set; }
+
+        [SmartResourceDisplayName("Plugins.ExternalAuth.ReadyConnect.ClientSecret")]
+        public string ClientSecret { get; set; }
+
+		[SmartResourceDisplayName("Plugins.ExternalAuth.ReadyConnect.RedirectUri")]
+		public string RedirectUrl { get; set; }
+	}
+}
---
+++ src/ReadySignOn.ReadyConnect/ReadyConnectExternalAuthSettings.cs
@@ -0,0 +1,14 @@
+﻿using SmartStore.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadySignOn.ReadyConnect
+{
+    public class ReadyConnectExternalAuthSettings : ISettings
+    {
+        public string ClientKeyIdentifier { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
