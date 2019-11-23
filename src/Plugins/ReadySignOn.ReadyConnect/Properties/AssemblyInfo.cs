d45a6a4c79934af2c29f4d2fbf5e47874e12392c
mridentity <mridentity>
2019-11-23 01:08:46 -0500

    Fix project settings for ReadyConnect.

--- src/Plugins/ReadySignOn.ReadyConnect/Properties/AssemblyInfo.cs
+++ src/Plugins/ReadySignOn.ReadyConnect/Properties/AssemblyInfo.cs
@@ -1,35 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
 [assembly: AssemblyTitle("ReadySignOn.ReadyConnect")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("ReadyConnect plug-in for SmartStore")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReadySignOn.ReadyConnect")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4975542b-0bb2-4e6a-8b9d-c7dfcaccb3f1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("8601ab38-769f-4eec-8d3f-a133fec6466d")]
