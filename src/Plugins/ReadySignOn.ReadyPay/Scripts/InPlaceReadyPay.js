4ae9144c1b45395a7c0a416dab22fdd6cc57eee3
mridentity <mridentity>
2019-12-24 01:32:42 -0500

    Add files from Wei for ReadyPay refactoring.

---
+++ src/Plugins/ReadySignOn.ReadyPay/Scripts/InPlaceReadyPay.js
@@ -0,0 +1,94 @@
+﻿// -----------------
+// Data
+// -----------------
+
+// selector for readypay container
+const divReadyPay = ".art-ready-pay";
+
+// selector for readypay button
+const btnReadyPay = "button";
+
+// selector for readyticket textbox
+const txtReadyTicket = "input[data-readyticket]";
+
+// destination url to post the ready ticket
+const destUrl = "https://reqres.in/api/users";
+
+// -----------------
+// Function
+// -----------------
+
+// getElementsBy consumes a selector: `s`;
+// it produces a list of elements selected by: `s`
+const getElementsBy = (s) => [...document.querySelectorAll(s)];
+
+// getSInP consumes a list parent element: `p`, a selector: `s`;
+// it produces the elment matching `s` under `p`
+const getSInP = (p, s) => p.querySelector(s);
+
+// -----------------
+// Main
+// -----------------
+
+getElementsBy(divReadyPay).map(div => {
+    const elemReadyPay = getSInP(div, btnReadyPay);
+    const elemReadyTicket = getSInP(div, txtReadyTicket);
+    const id = div.dataset.id;
+
+    // `bindReadyTicket` consumes an event: `e`;
+    // it sets the `readyticket` attribute of `e.target`
+    const bindReadyTicket = (e) => {
+        const elem = e.target;
+        const isValid = elem.value.length !== 0;
+        if (isValid && elemReadyPay.disabled) {
+            elemReadyPay.disabled = false;
+        } else if (!isValid && !elemReadyPay.disabled) {
+            elemReadyPay.disabled = true;
+        }
+        elem.dataset.readyticket = elem.value;
+    };
+
+
+    const bindReadyPay = (e) => {
+        // construct data to be posted
+        readyTicket = elemReadyTicket.dataset.readyticket;
+        const jsonData = {
+            id: id,
+            readyTicket: readyTicket
+        };
+
+        // construct settings for the request
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(jsonData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        // make the request; process the response: `res` if it succeeds;
+        // otherwise, logs errors to the console.
+        fetch(destUrl, options)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(res.status);
+                }
+            })
+            .then(res => console.log(res))
+            .catch(err => console.log(`Error when making request. ${err}`));
+    }
+
+    // disable ReadyPay button on load
+    elemReadyPay.disabled = true;
+
+    // set `data-readyticket` attribute each time users change the ready ticket number 
+    elemReadyTicket.addEventListener("input", bindReadyTicket);
+
+    // run `bindReadyPay` when users click readypay button 
+    elemReadyPay.addEventListener("click", bindReadyPay);
+
+});
+
+
