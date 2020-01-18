d95bc802689c63106be906ea40a4cc03df978d91
mridentity <mridentity>
2020-01-18 15:06:42 -0500

    Enable the use of ReadyID to register and login to SmartStore via ReadyConnect.

--- src/Plugins/ReadySignOn.ReadyConnect/Core/ReadyConnectProviderAuthorizer.cs
+++ src/Plugins/ReadySignOn.ReadyConnect/Core/ReadyConnectProviderAuthorizer.cs
@@ -179,7 +179,7 @@ namespace ReadySignOn.ReadyConnect.Core
 
             args.Add("client_id", settings.ClientId);
             args.Add("redirect_uri", GenerateLocalCallbackUri().AbsoluteUri);
-            args.Add("scope", "openid rso_idp email profile");
+            args.Add("scope", "openid rso_idp rso_rid email profile");
             args.Add("response_type", "code");
 
             AppendQueryArgs(builder, args);
